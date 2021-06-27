c42c55fe6d79c5ea1515634a076d6036ac88f60d
fausio luis <fausioluis@live.com>
2021-06-27 14:17:16 +0200

    Add project files.

---
+++ Domains/ActivistType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains
+{
+    public class ActivistType
+    {
+        protected int Id { get; set; }
+        protected string description { get; set; }
+    }
+}
---
+++ Domains/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains
+{
+    public abstract class Person
+    {
+        protected string Name { get; set; }
+        protected string Gender { get; set; }
+        protected DateTime DateOfBirth { get; set; }
+
+        protected string age()
+        {
+            // return the age
+            return null;
+        }
+    }
+}
